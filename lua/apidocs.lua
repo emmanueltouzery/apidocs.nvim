33ab0156885ebd9426728dedbfc83c075616097f
Emmanuel Touzery <etouzery@gmail.com>
2025-04-28 23:48:56 +0200

    work on following links as displayed at the bottom of docs

--- lua/apidocs.lua
+++ lua/apidocs.lua
@@ -1,8 +1,19 @@
+local function data_folder()
+  return vim.fn.stdpath("data") .. "/apidocs-data/"
+end
+
+-- https://stackoverflow.com/a/34953646/516188
+local function escape_pattern(text)
+    return text:gsub("([^%w])", "%%%1")
+end
+
+local function sanitize_fname(fname)
+  return fname:gsub("/", "_"):gsub("'", "_")
+end
+
 -- if the line contains table cells it's sensitive to alignment...
--- in that case compensate the neovim conceal that hides the ` characters
+-- in that case compensate the neovim conceal that hides the ` and other characters
 -- by adding extra spaces not to break the table borders alignment.
--- the ``` check.. unlikely a line has both ` and ```, lua regexes are
--- painful and i don't want to break ``` patterns adding spaces.
 local function add_spaces_to_compensate_conceals_cols(lines)
   local lines_str = vim.fn.join(lines, "\n")
 
@@ -101,6 +112,134 @@ local function add_spaces_to_compensate_conceals_cols(lines)
   return lines
 end
 
+local function urldecode(url)
+  return url:gsub("%%20", " "):gsub("%%3c", "<"):gsub("%%3e", ">"):gsub("%%23", "#")
+end
+
+local function fix_file_links(fname, lines, target_path, choice, path_to_name,
+    name_and_id_to_string_nearby, orig_path)
+  local changes = false
+  for i = #lines, 1, -1 do
+    local l, m = lines[i]:match("^( +%d+%. )(.*)$")
+    -- remove the path prefix, which could be the folder in which we store the files, or
+    -- any parent of it, in case it's a link to '../../filename'
+    if m ~= nil and m:match("^file://") then
+      local file_guessed_subpath_str = orig_path:gsub("/[^/]+$", "") -- take the parent the first time, it's the filename
+      if not orig_path:match("/") then
+        -- completing the gsub before.. no child folder. remove the filename
+        file_guessed_subpath_str = ""
+      end
+      if m:match("getattribute") then
+        print("m " .. m)
+        print("orig_path " .. orig_path)
+      end
+      local prefix = "file://" .. target_path
+      -- if the link points to target_path/orig_subfolder/../../ then i must use orig_path/../../
+      while #prefix > 0 do
+        if m:match("getattribute") then
+          print("prefix " .. prefix)
+          print("file_guessed_subpath_str " .. file_guessed_subpath_str)
+        end
+        -- take the parent folder of the prefix until it is a prefix of m.
+        if m:match("^" .. escape_pattern(prefix)) then
+          break
+        end
+        -- everytime i take the parent of prefix, take the parent of orig_path too
+        prefix = prefix:gsub("/[^/]+$", "")
+        if file_guessed_subpath_str:match("/") then
+          file_guessed_subpath_str = file_guessed_subpath_str:gsub("/[^/]+$", "")
+        else
+          file_guessed_subpath_str = ""
+        end
+      end
+      if m:match("getattribute") then
+        print("final file_guessed_subpath_str " .. file_guessed_subpath_str)
+      end
+
+      local link_target = urldecode(m):gsub("^" .. escape_pattern(prefix), "")
+      if m:match("getattribute") then
+        print("link_target " .. link_target)
+      end
+      local file_id = vim.split(link_target, "#")
+      if #file_id == 4 then
+        -- for lua, the path sometimes contains a #...
+        -- it's a link to the same file, which was already properly named... "name#pa#th#id"
+        local path = sanitize_fname(file_id[1]:gsub("^/", "") .. "#" .. file_id[2] .. "#" .. file_id[3]:gsub("%.html$", ""))
+        if path ~= nil then
+          if name_and_id_to_string_nearby[path] ~= nil then
+            local text_section = name_and_id_to_string_nearby[path][file_id[4]]
+            if text_section ~= nil then
+              lines[i] = l .. "local://" .. choice .. "/" .. sanitize_fname(path) .. "#" .. text_section
+              changes = true
+            end
+          -- elseif #vim.tbl_keys(name_and_id_to_string_nearby) == 1 then
+          --   local text_section = name_and_id_to_string_nearby[vim.tbl_keys(name_and_id_to_string_nearby)[1]][file_id[4]]
+          --   if text_section ~= nil then
+          --     lines[i] = l .. "local://" .. choice .. "/" .. sanitize_fname(path) .. "#" .. text_section
+          --     changes = true
+          --   end
+          end
+        end
+      elseif #file_id == 3 then
+        -- it's a link to the same file, which was already properly named... "name#path#id"
+        local path = sanitize_fname(file_id[1]:gsub("^/", "") .. "#" .. file_id[2]:gsub("%.html$", ""))
+        if path ~= nil and name_and_id_to_string_nearby[path] ~= nil then
+          local text_section = name_and_id_to_string_nearby[path][file_id[3]]
+          if text_section ~= nil then
+            lines[i] = l .. "local://" .. choice .. "/" .. sanitize_fname(path) .. "#" .. text_section
+            changes = true
+          end
+        end
+      elseif #file_id == 2 then
+        -- link to another file, ID lookup
+        local name = path_to_name[file_id[1]:gsub("^/", "")]
+        if name ~= nil then
+          local text_section = name_and_id_to_string_nearby[sanitize_fname(name .. "#" .. file_id[1]:gsub("^/", ""))][file_id[2]]
+          if text_section ~= nil then
+            lines[i] = l .. "local://" .. choice .. "/" .. sanitize_fname(sanitize_fname(name .. "#" .. file_id[1]:gsub("^/", "")) .. "#" .. text_section)
+            changes = true
+          end
+        end
+      else
+        -- link to a pain file, no ID lookup
+        -- print("will search for: '" .. file_guessed_subpath_str .. link_target .. "'")
+        -- print(vim.inspect(path_to_name))
+        -- print(vim.inspect(path_to_name))
+        -- print(file_guessed_subpath_str)
+        link_target = link_target:gsub("^/", "")
+        local name = path_to_name[file_guessed_subpath_str .. "/" .. link_target] or path_to_name[link_target]
+
+        -- if m:match("getattribute") then
+        --   print("name " .. name)
+        --   print("file_guessed_subpath_str " .. file_guessed_subpath_str)
+        --   print("link_target " .. link_target)
+        -- end
+        if name ~= nil then
+          if #file_guessed_subpath_str > 0 then
+            lines[i] = l .. "local://" .. choice .. "/" .. sanitize_fname(name .. "#" .. file_guessed_subpath_str .. "/" .. link_target)
+          else
+            lines[i] = l .. "local://" .. choice .. "/" .. sanitize_fname(name .. "#" .. link_target)
+          end
+          if m:match("getattribute") then
+            print(lines[i])
+          end
+          changes = true
+        -- else
+        --   print(m)
+        --   print(vim.inspect(path_to_name))
+        --   print("NOT FOUND")
+        --   print("prefix => " .. prefix)
+        --   print("m => " .. m)
+        --   print("file_guessed_subpath_str: " .. file_guessed_subpath_str)
+        --   print("link_target: " .. link_target)
+        --   print("orig_path: " .. orig_path)
+        end
+      end
+    end
+  end
+  return lines, changes
+end
+
 local function html_extra_css(source)
   if source:match("openjdk") then
     return [[
@@ -120,10 +259,6 @@ local function html_extra_css(source)
   else
     return ""
   end
-end
-
-local function data_folder()
-  return vim.fn.stdpath("data") .. "/apidocs-data/"
 end
 
 local function apidoc_install(choice, slugs_to_mtimes, cont)
@@ -151,7 +286,7 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
       local file_id = vim.split(entry.path, "#")
       if #file_id == 2 then
         path_to_name[entry.path] = entry.name
-        local sanitized_fname = file_id[1]:gsub("/", "_")
+        local sanitized_fname = sanitize_fname(file_id[1])
         if known_keys_per_path[file_id[1]] == nil then
           known_keys_per_path[file_id[1]] = {[file_id[2]] = true}
         else
@@ -165,9 +300,15 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
       local target_path = data_folder .. choice
       vim.system({"sh", "-c", "rm -Rf " .. target_path}):wait()
       vim.fn.mkdir(target_path, "p")
+      -- used to split files in sections based on ids referenced from the toplevel
       local name_and_id_to_pos = {}
+      -- used to gather all section "titles" so that we can prepare links to this
+      -- part of the files later on. So we gather this for ALL ids, whether we know
+      -- about them or not.
+      local name_and_id_to_string_nearby = {}
       local name_known_byte_offsets = {}
       local name_to_contents = {}
+      local out_path_to_orig_path = {}
 
       local query = vim.treesitter.query.parse('html', [[
       (attribute
@@ -180,7 +321,8 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
 
     -- save all the files
     for _, key in ipairs(vim.tbl_keys(data)) do
-      local sanitized_key = ((path_to_name[key] or key) .. "#" .. key):gsub("/", "_")
+      local sanitized_key = sanitize_fname((path_to_name[key] or key) .. "#" .. key)
+      out_path_to_orig_path[sanitized_key .. ".html"] = key
       local file = io.open(target_path .. "/" .. sanitized_key  .. ".html", "w")
       contents = data[key]
       :gsub("<pre([^>]*)>(.-)</pre>", function(pre_attrs, children)
@@ -229,16 +371,23 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
 
       name_to_contents[sanitized_key] = contents
       name_and_id_to_pos[sanitized_key] = {}
+      name_and_id_to_string_nearby[sanitized_key] = {}
       name_known_byte_offsets[sanitized_key] = {#contents}
 
       local start_ids = vim.loop.hrtime()
-      if known_keys_per_path[key] ~= nil then
-        for id, node, metadata in query:iter_captures(tree:root(), contents) do
-          id_val = vim.treesitter.get_node_text(node:next_named_sibling():named_child(), contents)
-          if known_keys_per_path[key][id_val] then
+      for id, node, metadata in query:iter_captures(tree:root(), contents) do
+        if node:next_named_sibling():named_child_count() > 0 then
+          local id_val = vim.treesitter.get_node_text(node:next_named_sibling():named_child(), contents)
+          if known_keys_per_path[key] ~= nil and known_keys_per_path[key][id_val] then
             _, _, byte_pos = node:parent():parent():start()
             name_and_id_to_pos[sanitized_key][id_val] = byte_pos
             table.insert(name_known_byte_offsets[sanitized_key], byte_pos)
+          end
+          if node:parent() ~= nil and node:parent():parent() ~= nil
+            and node:parent():parent():next_named_sibling() ~= nil
+            and node:parent():parent():next_named_sibling():type() == "text" then
+            name_and_id_to_string_nearby[sanitized_key][id_val] =
+            vim.treesitter.get_node_text(node:parent():parent():next_named_sibling(), contents)
           end
         end
       end
@@ -253,9 +402,9 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
     local start_writing = vim.loop.hrtime()
     for path, name in pairs(path_to_name) do
       local file_id = vim.split(path, "#")
-      local sanitized_fname = name:gsub("/", "_")
+      local sanitized_fname = sanitize_fname(name)
       if #file_id == 2 then
-        local sanitized_containing_file_name = ((path_to_name[file_id[1]] or file_id[1]) .. "#" .. file_id[1]):gsub("/", "_")
+        local sanitized_containing_file_name = sanitize_fname((path_to_name[file_id[1]] or file_id[1]) .. "#" .. file_id[1])
         local byte = name_and_id_to_pos[sanitized_containing_file_name][file_id[2]]
         local to_write_contents = nil
         if byte == nil then
@@ -271,8 +420,10 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
           end
           to_write_contents = string.sub(name_to_contents[sanitized_containing_file_name], byte, next_byte)
         end
-        local sanitized_name = name:gsub("/", "_")
-        local file = io.open((target_path .. "/" .. sanitized_name .. "#" .. path:gsub("/", "_")):sub(1, 250) .. ".html", "w")
+        local sanitized_name = sanitize_fname(name)
+        local out_path = sanitize_fname(sanitized_name .. "#" .. path):sub(1, 250) .. ".html"
+        out_path_to_orig_path[out_path] = path
+        local file = io.open(target_path .. "/" .. out_path, "w")
         file:write(html_extra_css(choice))
         if path_to_type[file_id[1]] ~= nil then
           file:write("> " .. path_to_type[file_id[1]] .. "/" .. path_to_name[file_id[1]] .. "\n")
@@ -287,13 +438,14 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
     -- convert the html to text, on 8 processes concurrently (-P8)
     vim.system({
       "sh", "-c",
-      [[find . -maxdepth 1 -name '*.html' -print0 | xargs -0 -P 8 -I param sh -c "elinks -config-dir ]] .. data_folder .. [[ -dump 'param' > 'param'.md && rm 'param'"]]
+      -- [[find . -maxdepth 1 -name '*.html' -print0 | xargs -0 -P 8 -I param sh -c "elinks -config-dir ]] .. data_folder .. [[ -dump 'param' > 'param'.md && rm 'param'"]]
+      [[find . -maxdepth 1 -name '*.html' -print0 | xargs -0 -P 8 -I param sh -c "elinks -config-dir ]] .. data_folder .. [[ -dump 'param' > 'param'.md"]]
     }, {cwd=target_path}):wait()
     local elapsed_elinks = (vim.loop.hrtime() - start_elinks) / 1e9
 
     local start_pp = vim.loop.hrtime()
 
-    -- unfortunately i must post-process the markdown to fix conceal table alignment..
+    -- unfortunately i must post-process the markdown to fix conceal table alignment and fix links..
     vim.system({"rg", "-l", "│"}, {cwd=target_path}, vim.schedule_wrap(function(res)
       for _, fname in ipairs(vim.fn.split(res.stdout, "\n")) do
         local filepath = target_path .. "/" .. fname
@@ -302,8 +454,29 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
           table.insert(lines, line)
         end
         local file = io.open(filepath, "w")
-        file:write(vim.fn.join(add_spaces_to_compensate_conceals_cols(lines), "\n"))
+        local after_conceal = add_spaces_to_compensate_conceals_cols(lines)
+        file:write(vim.fn.join(after_conceal, "\n"))
         file:close()
+      end
+
+      local fs = vim.uv.fs_scandir(target_path)
+      while true do
+        local name, type = vim.uv.fs_scandir_next(fs)
+        if not name then break end
+        if type ~= 'directory' then
+          local filepath = target_path .. "/" .. name
+          local lines = {}
+          for line in io.lines(filepath) do
+            table.insert(lines, line)
+          end
+          local after_links, changes = fix_file_links(
+            filepath, lines, target_path, choice, path_to_name, name_and_id_to_string_nearby, out_path_to_orig_path[name:gsub(".md$", "")])
+          if changes then
+            local file = io.open(filepath, "w")
+            file:write(vim.fn.join(after_links, "\n"))
+            file:close()
+          end
+        end
       end
 
       local elapsed_pp = (vim.loop.hrtime() - start_pp) / 1e9
@@ -358,6 +531,23 @@ local function load_doc_in_buffer(buf, filepath)
   else
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "File not readable: " .. filepath })
   end
+end
+
+local function buf_view_switch_to_new(new_buf)
+  vim.wo.winfixbuf = false
+  vim.api.nvim_win_set_buf(0, new_buf)
+  vim.api.nvim_buf_set_option(0, 'modifiable', false)
+  vim.wo.winfixbuf = true
+  vim.wo.wrap = false
+  vim.bo.modified = false
+
+  vim.keymap.set("n", "<C-o>", function()
+    vim.wo.winfixbuf = false
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-o>", true, false, true), "n", true)
+    vim.defer_fn(function()
+      vim.wo.winfixbuf = true
+    end, 100)
+  end, {buffer = true})
 end
 
 local function apidocs_open(params, slugs_to_mtimes)
@@ -473,6 +663,39 @@ local function apidocs_open(params, slugs_to_mtimes)
         vim.api.nvim_buf_set_option(0, 'modifiable', false)
         vim.wo.wrap = false
         vim.bo.modified = false
+
+        vim.keymap.set("n", "<C-]>", function()
+          local line = vim.api.nvim_buf_get_lines(0, vim.fn.line(".")-1, vim.fn.line("."), false)[1]
+          -- print(line)
+          local m = string.match(line, "^%s+%d+%. local://")
+          if m then
+            local target = line:sub(#m+1)
+            local components = vim.split(target, "#")
+            print(vim.inspect(components))
+            -- TODO allow C-o navigation, ie open a new buffer instead of replacing the contents of this one
+            if #components == 2 then
+              -- plain file name
+              local new_buf = vim.api.nvim_create_buf(true, false)
+              load_doc_in_buffer(new_buf, data_folder() .. target .. ".html.md")
+              buf_view_switch_to_new(new_buf)
+
+            elseif #components == 3 then
+              -- file name+section ID
+              local new_buf = vim.api.nvim_create_buf(true, false)
+              load_doc_in_buffer(new_buf, data_folder() .. components[1] .. "#" .. components[2] .. ".html.md")
+              buf_view_switch_to_new(new_buf)
+              vim.cmd("/" .. components[3])
+
+            elseif #components == 4 then
+              -- file name with two hashes+section ID (happens for lua)
+              local new_buf = vim.api.nvim_create_buf(true, false)
+              print(data_folder() .. components[1] .. "#" .. components[2] .. "#" .. components[3] .. ".html.md")
+              load_doc_in_buffer(new_buf, data_folder() .. components[1] .. "#" .. components[2] .. "#" .. components[3] .. ".html.md")
+              buf_view_switch_to_new(new_buf)
+              vim.cmd("/" .. components[4])
+            end
+          end
+        end, {buffer = true})
       end)
       return true
     end,
