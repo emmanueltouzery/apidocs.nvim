30f0bbe0e37ca2371173c8ef9796e90ae5ab7acb
Emmanuel Touzery <etouzery@gmail.com>
2025-05-02 14:11:41 +0200

    fix removing the intermediary HTML files...
    they'll show up in the search otherwise

--- lua/apidocs/install.lua
+++ lua/apidocs/install.lua
@@ -469,8 +469,8 @@ local function apidoc_install(choice, slugs_to_mtimes, cont)
     -- convert the html to text, on 8 processes concurrently (-P8)
     vim.system({
       "sh", "-c",
-      -- [[find . -maxdepth 1 -name '*.html' -print0 | xargs -0 -P 8 -I param sh -c "elinks -config-dir ]] .. data_folder .. [[ -dump 'param' > 'param'.md && rm 'param'"]]
-      [[find . -maxdepth 1 -name '*.html' -print0 | xargs -0 -P 8 -I param sh -c "elinks -config-dir ]] .. data_folder .. [[ -dump 'param' > 'param'.md"]]
+      [[find . -maxdepth 1 -name '*.html' -print0 | xargs -0 -P 8 -I param sh -c "elinks -config-dir ]] .. data_folder .. [[ -dump 'param' > 'param'.md && rm 'param'"]]
+      -- [[find . -maxdepth 1 -name '*.html' -print0 | xargs -0 -P 8 -I param sh -c "elinks -config-dir ]] .. data_folder .. [[ -dump 'param' > 'param'.md"]]
     }, {cwd=target_path}):wait()
     local elapsed_elinks = (vim.loop.hrtime() - start_elinks) / 1e9
 
