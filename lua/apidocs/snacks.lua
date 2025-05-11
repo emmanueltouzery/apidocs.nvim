f464e1c4562d2aa106c79ab4a18311d1b5bd7ad5
hnolan <hayden@hnolan.ca>
2025-05-11 16:03:51 -0400

    Refactor get_data_dirs to only initiate extra loop if no exact match is found

--- lua/apidocs/snacks.lua
+++ lua/apidocs/snacks.lua
@@ -28,11 +28,12 @@ local function get_data_dirs(opts)
   local dirs = {}
   local install_dirs = vim.fn.readdir(data_dir)
   for _, source in ipairs(opts.restrict_sources) do
-    for _, entry in ipairs(install_dirs) do
-      if vim.fn.isdirectory(data_dir .. entry) == 1 then
-        if entry == source then
-          table.insert(dirs, data_dir .. entry .. "/")
-        elseif entry:match("^" .. source .. "~%d+(%.%d+)?$") then
+    local dir = data_dir .. source .. "/"
+    if vim.fn.isdirectory(dir) == 1 then
+      table.insert(dirs, dir)
+    else
+      for _, entry in ipairs(install_dirs) do
+        if entry:match("^" .. source .. "~%d+") then
           -- check for partial match
           -- e.g. "python" -> "python~3.12"
           table.insert(dirs, data_dir .. entry .. "/")
